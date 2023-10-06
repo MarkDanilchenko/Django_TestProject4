47c3a0d6598323e888bcf94e5c2a60b9591091db
MarkDanilchenko <my.ucheba.2023@gmail.com>
2023-10-07 00:18:50 +0300

    Add scss, js and html for main page.

--- static/webpack.config.js
+++ static/webpack.config.js
@@ -1,26 +1,26 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: {
-        main: path.resolve(__dirname, './src/js/main.js')
-    },
-    mode: 'production',
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            },
-            {
-                test: /\.css$/,
-                exclude: /node_modules/,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    }
-}
+  entry: {
+    main: path.resolve(__dirname, "./src/js/main.js"),
+  },
+  mode: "production",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
